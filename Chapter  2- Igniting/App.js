09ceac2b7692b643c744d0c3c278d3b7dcd2bec5
ajaythopate <ajaythopate999@gmail.com>
2023-08-18 20:07:46 +0530

    episode 2

---
+++ Chapter  2- Igniting/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h2", {}, "I am a h2 tag"),
+
+    ]),
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "I am a h1 tag"),
+        React.createElement("h2", {}, "I am a h2 tag"),
+    ]),
+]);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
